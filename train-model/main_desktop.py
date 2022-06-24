83488842a9f2f2de21c8b854d7ea6776236b2e73
daothanhhuy <19521601@gm.uit.edu.vn>
2022-06-24 11:28:11 +0700

    Chinh sua get dia chi IP

--- train-model/main_desktop.py
+++ train-model/main_desktop.py
@@ -21,9 +21,20 @@ refreshDeviceApi = 'http://localhost:3000/refresh'
 refreshDelay = 180 # 3 phut
 
 def get_current_ip():
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
-    return IPAddr
+    # hostname = socket.getfqdn()
+    # IPAddr = socket.gethostbyname(hostname)
+    # return IPAddr
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+            # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def start_thread(func, args):
     p = threading.Thread(target= func, args = args, daemon=True)
