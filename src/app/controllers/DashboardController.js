be846e53d705f39a638ec532e5e1e55420d8e77c
Tran Cuong <tranquocc1@gmail.com>
2022-06-26 20:51:29 +0700

    Delete some comments

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -19,6 +19,7 @@ class DashboardController {
             }
             let updatedAt = result.updatedAt.toLocaleDateString();
             devices.push({
+                _id: result._id,
                 name: result.name,
                 imgUrl: result.imgUrl,
                 updatedAt: updatedAt,
@@ -31,6 +32,14 @@ class DashboardController {
             title: title 
         });
         } else res.redirect('/');
+    }
+
+    // [DELETE] dashboard/delete/:id
+    destroy(req, res, next) {
+        console.log(req.params.id);
+        Device.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('/dashboard'))
+            .catch(next);
     }
 }
 
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const dashboardController = require('../app/controllers/DashboardController');
 
+router.delete('delete/:id', dashboardController.destroy);
 router.get('/', dashboardController.index);
 
 module.exports = router;
